57eb0e0c4ae6c4b29277e73e2f23c64cfbcf2b35
ajama <ajama@kth.se>
2023-02-19 19:59:05 +0100

    Test #13: add 4 tests to improve Branch coverage.

--- javaparser-core-testing/src/test/java/com/github/javaparser/ast/expr/ObjectCreationExprTest.java
+++ javaparser-core-testing/src/test/java/com/github/javaparser/ast/expr/ObjectCreationExprTest.java
@@ -21,9 +21,14 @@
 
 package com.github.javaparser.ast.expr;
 
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.NodeList;
+import com.github.javaparser.ast.body.BodyDeclaration;
 import com.github.javaparser.utils.TestParser;
 import org.junit.jupiter.api.Test;
 
+import static com.github.javaparser.StaticJavaParser.parseBodyDeclaration;
+import static com.github.javaparser.StaticJavaParser.parseExpression;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ObjectCreationExprTest {
@@ -31,5 +36,83 @@ class ObjectCreationExprTest {
     void aaa() {
         Expression e = TestParser.parseExpression("new @Test N()");
         assertEquals("new @Test N()", e.toString());
+    }
+
+    @Test
+    void BodyHasSameNameAsClass() {
+        ObjectCreationExpr objectCreationExpr = new ObjectCreationExpr();
+        ObjectCreationExpr oc2 = new ObjectCreationExpr();
+        NodeList<Expression> list1 = new NodeList<>();
+        list1.add(parseExpression("new OtherClass()"));
+        list1.add(parseExpression("new InnerClass()"));
+        objectCreationExpr.setArguments(list1);
+
+        NodeList<Expression> list2 = new NodeList<>();
+        list2.add(parseExpression("new IntegerClass()"));
+        oc2.setArguments(list2);
+
+        NodeList<BodyDeclaration<?>> body = new NodeList<>();
+        body.add(parseBodyDeclaration("int x = 0;"));
+        objectCreationExpr.setAnonymousClassBody(body);
+
+        Node node = objectCreationExpr.getAnonymousClassBody().get().get(0);
+        Node Replacement = objectCreationExpr.getAnonymousClassBody().get().get(0);
+
+        assertEquals(true, objectCreationExpr.replace(node,Replacement));
+    }
+
+    @Test
+    void nodeisNull() {
+        ObjectCreationExpr objectCreationExpr = new ObjectCreationExpr();
+        ObjectCreationExpr oc2 = new ObjectCreationExpr();
+        NodeList<Expression> list1 = new NodeList<>();
+        list1.add(parseExpression("new OtherClass()"));
+        list1.add(parseExpression("new InnerClass()"));
+        objectCreationExpr.setArguments(list1);
+
+        NodeList<Expression> list2 = new NodeList<>();
+        list2.add(parseExpression("new IntegerClass()"));
+        oc2.setArguments(list2);
+
+        NodeList<BodyDeclaration<?>> body = new NodeList<>();
+        body.add(parseBodyDeclaration("int x = 0;"));
+        objectCreationExpr.setAnonymousClassBody(body);
+
+        Node node = null;
+        Node Replacement = objectCreationExpr.getAnonymousClassBody().get().get(0);
+
+        assertEquals(false, objectCreationExpr.replace(node,Replacement));
+    }
+
+    @Test
+    void argumentEqualsNode() {
+        ObjectCreationExpr objectCreationExpr = new ObjectCreationExpr();
+        ObjectCreationExpr oc2 = new ObjectCreationExpr();
+        NodeList<Expression> list1 = new NodeList<>();
+        list1.add(parseExpression("new OtherClass()"));
+        list1.add(parseExpression("new InnerClass()"));
+        objectCreationExpr.setArguments(list1);
+
+        NodeList<Expression> list2 = new NodeList<>();
+        list2.add(parseExpression("new IntegerClass()"));
+        oc2.setArguments(list2);
+
+        Node node = objectCreationExpr.getArguments().get(0);
+        Node Replacement = oc2.getArguments().get(0).asObjectCreationExpr();
+
+        assertEquals(true, objectCreationExpr.replace(node,Replacement));
+    }
+
+    @Test
+    void scopeEqualsNode() {
+        ObjectCreationExpr objectCreationExpr = new ObjectCreationExpr();
+        ObjectCreationExpr oc2 = new ObjectCreationExpr();
+        objectCreationExpr.setScope(parseExpression("new OtherClass()"));
+        oc2.setScope(parseExpression("new IntegerClass()"));
+
+        Node node = objectCreationExpr.getScope().get();
+        Node Replacement = oc2.getScope().get();
+
+        assertEquals(true, objectCreationExpr.replace(node,Replacement));
     }
 }
